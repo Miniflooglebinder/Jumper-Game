f48de0b075b5a88a95a20415eb1ac9865b89469c
Miniflooglebinder <seth.horgan@gmail.com>
2023-07-03 22:19:05 +0800

    Update: Jump
    PlayerJump script added

---
+++ Jumper/Assets/Scripts/Player/PlayerJump.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float jumpForce = 18f;
+    public float gravityBase = 30f;
+    public float gravityExpontent = 2f;
+    public float maxGravity = 70f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void jump()
+    {
+        rb.velocity = Vector3.up * jumpForce;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float gravity = gravityBase * Mathf.Pow(rb.velocity.y, gravityExpontent);
+        gravity = Mathf.Clamp(gravity, 0f, maxGravity);
+        rb.AddForce(Vector3.down * gravity * rb.mass);
+    }
+}
