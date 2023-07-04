c59db7f3925f1dadf43eeaabcd4af02213ff6a08
Miniflooglebinder <seth.horgan@gmail.com>
2023-07-04 22:44:15 +0800

    Highscore SFX

--- Jumper/Assets/Scripts/UI/ScoreText.cs
+++ Jumper/Assets/Scripts/UI/ScoreText.cs
@@ -6,6 +6,7 @@ public class ScoreText : MonoBehaviour
 {
     private TextMeshProUGUI scoreText; // Reference to the TextMeshProUGUI component on this object
     private int score = 0;
+    public bool hasBeatenHighScore = false;
 
     private TextMeshProUGUI highScoreText;
 
@@ -25,6 +26,12 @@ public class ScoreText : MonoBehaviour
 
         if (score > PlayerPrefs.GetInt("HighScore", 0)) // If the current score is greater than the high score
         {
+            if(!hasBeatenHighScore)
+            {
+                FindObjectOfType<AudioManager>().Play("HighScore");
+                hasBeatenHighScore = true;
+            }
+
             PlayerPrefs.SetInt("HighScore", score); // Set the high score to the current score
             highScoreText.text = "Highscore: " + score.ToString();
         }
